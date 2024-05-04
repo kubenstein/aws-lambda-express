e49b70eb8f6778b166f9350239b240df2f3b4972
Jakub Niewczas <niewczas.jakub@gmail.com>
2024-05-04 23:26:44 +0900

    wip

--- lambda-entry-point.js
+++ lambda-entry-point.js
@@ -1,3 +1,88 @@
-const serverlessExpress = require("@vendia/serverless-express");
-const app = require("./src/");
-exports.handler = serverlessExpress({ app });
+const {
+  DynamoDBClient,
+  PutItemCommand,
+} = require("@aws-sdk/client-dynamodb");
+
+const dynamodbClient = new DynamoDBClient({
+  endpoint: process.env.DYNAMODB_ENDPOINT,
+});
+
+exports.handler = async event => {
+  try {
+
+    const input = {
+      "Item": {
+        "AlbumTitle": {
+          "S": "Somewhat Famous"
+        },
+        "Artist": {
+          "S": "No One You Know"
+        },
+        "SongTitle": {
+          "S": "Call Me Today"
+        }
+      },
+      "ReturnConsumedCapacity": "TOTAL",
+      "TableName": "YourTableName"
+    };
+
+    const putItemCommand = new PutItemCommand(input);
+    await dynamodbClient.send(putItemCommand);
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: "Data added to DynamoDB table" }),
+    };
+  } catch (error) {
+    console.error("Error:", error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: "An error occurred" }),
+    };
+  }
+};
+
+// exports.handler = async (event) => {
+//   // Your Lambda function logic goes here
+//   const response = {
+//       statusCode: 200,
+//       headers: {
+//           "Content-Type": "application/json"
+//       },
+//       body: JSON.stringify({
+//           message: "Hello from Lambda!"
+//       })
+//   };
+//   return response;
+// };
+
+// const params = {
+//   TableName: process.env.TABLE_NAME,
+//   Item: {
+//     id: 1,
+//     data: "hello",
+//   },
+// };
+
+// try {
+//   // Perform a put operation to insert data into the DynamoDB table
+//   await dynamoDb.put(params).promise();
+
+//   // Return a success response
+//   return {
+//     statusCode: 200,
+//     body: JSON.stringify({
+//       message: "Data inserted successfully.",
+//     }),
+//   };
+// } catch (error) {
+//   console.error(error);
+
+//   // Return an error response
+//   return {
+//     statusCode: 500,
+//     body: JSON.stringify({
+//       message: "Error occurred while inserting data into DynamoDB table.",
+//     }),
+//   };
+// }
+// };
--- scripts/run-dev-env-docker.sh
+++ scripts/run-dev-env-docker.sh
@@ -7,13 +7,16 @@ COMMAND=$@
 
 DOCKERFILE=$(cat <<FILE
   FROM node:18-alpine
+  ENV SAM_CLI_TELEMETRY=0
+  ENV AWS_ACCESS_KEY_ID=DEV
+  ENV AWS_SECRET_ACCESS_KEY=DEV
+  ENV AWS_DEFAULT_REGION=DEV
   RUN apk update \
    && apk add musl-dev gcc python3 python3-dev bash \
    && rm -rf /usr/lib/python3.11/EXTERNALLY-MANAGED \
    && python3 -m ensurepip --upgrade \
    && pip3 install --upgrade pip \
    && pip3 install --upgrade awscli aws-sam-cli
-  ENV SAM_CLI_TELEMETRY=0
   EXPOSE 3000
 FILE
 )
@@ -24,10 +27,10 @@ docker run -i --rm \
   $([ -z "$NO_TTY" ] && echo '-t') \
   -e CI \
   -e HOST_PWD=$(pwd) \
-  -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID \
-  -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY \
+  $([ -n "$AWS_ACCESS_KEY_ID" ] && echo "-e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID") \
+  $([ -n "$AWS_SECRET_ACCESS_KEY" ] && echo "-e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY") \
   -v /var/run/docker.sock:/var/run/docker.sock \
   -v $(pwd):/$(pwd) \
   -p 3000:3000 \
   aws-sam-dev-env \
-  sh -l -c "cd \$HOST_PWD ; yarn ; ${COMMAND:-./scripts/run-dev-env.sh}"
+  sh -l -c "cd \$HOST_PWD ; ${COMMAND:-./scripts/run-dev-env.sh}"
